d0b440198b8e50a059c450259edb2d7348563f0f
arsema619 <arsemamenberu16@gmail.com>
2026-04-01 14:15:27 -0700

    Update urls.py

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+     path("", include("study.urls")),
     path('admin/', admin.site.urls),
-    path("", include("study.urls")),
+    path('api/', include('progress.urls')),
 ]
