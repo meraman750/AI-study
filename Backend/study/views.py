5ce9d24a18070f562f5b61cfe1cd5bf8df99cfe1
arsema619 <arsemamenberu16@gmail.com>
2026-04-01 14:13:22 -0700

    Update views.py

--- Backend/study/views.py
+++ Backend/study/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Progress
 
-# Create your views here.
+def get_progress(request):
+
+    progress_data = Progress.objects.all()
+
+    data = []
+
+    for p in progress_data:
+        data.append({
+            "user": p.user.username,
+            "subject": p.subject,
+            "quizzes_taken": p.quizzes_taken,
+            "average_score": p.average_score
+        })
+
+    return JsonResponse(data, safe=False)
