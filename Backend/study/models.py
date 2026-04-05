9263d885f032b87833990cb8594e6816345b4684
Meheretabe Abayneh <meraman675@gmail.com>
2026-04-05 19:33:37 +0300

    Table editting for the AI part

--- Backend/study/models.py
+++ Backend/study/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from file_process.models import StudyMaterial
 
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True)
+# # Create your models here.
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     joined_at = models.DateTimeField(auto_now_add=True)
 
-class Subject(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+# class Subject(models.Model):
+#     name = models.CharField(max_length=100, unique=True)
 
-class StudyMaterial(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)  # Owner of the material
-    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)  # Related subject
-    title = models.CharField(max_length=255)  # Material title
-    file = models.FileField(upload_to="materials/")  # Uploaded file (PDF/DOC)
-    extracted_text = models.TextField(blank=True)  # Text extracted for AI processing
-    uploaded_at = models.DateTimeField(auto_now_add=True)  # Upload timestamp
+# class StudyMaterial(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)  # Owner of the material
+#     subject = models.ForeignKey(Subject,on_delete=models.CASCADE)  # Related subject
+#     title = models.CharField(max_length=255)  # Material title
+#     file = models.FileField(upload_to="materials/")  # Uploaded file (PDF/DOC)
+#     extracted_text = models.TextField(blank=True)  # Text extracted for AI processing
+#     uploaded_at = models.DateTimeField(auto_now_add=True)  # Upload timestamp
 
 class Summary(models.Model):
     material = models.OneToOneField(
@@ -52,27 +53,27 @@ class Question(models.Model):
 
 # Quiz → Many Questions
 
-class QuizAttempt(models.Model):
-    # Used For
-        # Progress dashboard
-        # Statistics
-        # Average score
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+# class QuizAttempt(models.Model):
+#     # Used For
+#         # Progress dashboard
+#         # Statistics
+#         # Average score
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
-    score = models.IntegerField()
-    total_questions = models.IntegerField()
+#     score = models.IntegerField()
+#     total_questions = models.IntegerField()
 
-    attempted_at = models.DateTimeField(auto_now_add=True)
+#     attempted_at = models.DateTimeField(auto_now_add=True)
 
-class Progress(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
+# class Progress(models.Model):
+#     user = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE
+#     )
 
-    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
-    quizzes_taken = models.IntegerField(default=0)
-    average_score = models.FloatField(default=0)
-    last_updated = models.DateTimeField(auto_now=True)
+#     subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+#     quizzes_taken = models.IntegerField(default=0)
+#     average_score = models.FloatField(default=0)
+#     last_updated = models.DateTimeField(auto_now=True)
 
