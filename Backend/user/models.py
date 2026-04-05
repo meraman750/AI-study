394e63262535d55556a6021a7c370929ed350e53
Nardos Assefa Gonfa <nardiassefa97@gmail.com>
2026-04-06 01:10:56 +0300

    Add UserProfile model with username, email, and password

--- Backend/user/models.py
+++ Backend/user/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
